e3a5940cc601cfd20c7daaf5e6045fe4456b083a
dg8fv2010 <945920259@qq.com>
2015-10-04 20:45:23 +0800

    Code

---
+++ 226.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+struct TreeNode {
+ int val;
+ TreeNode *left;
+ TreeNode *right;
+ TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root==NULL)
+        {
+             return NULL;
+        }
+        TreeNode* tmp;
+        tmp=root->left;
+        root->left=root->right;
+        root->right=tmp;
+        root->left=invertTree(root->left);
+        root->right=invertTree(root->right);
+
+        return root;
+    }
+};
---
+++ 235.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+
+using namespace std;
+
+struct TreeNode {
+ int val;
+ TreeNode *left;
+ TreeNode *right;
+ TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root==NULL)
+        {
+            return NULL;
+        }
+        if (p!=NULL && q==NULL)
+        {
+            return NULL;
+        }
+        if (p==NULL && q!=NULL)
+        {
+            return NULL;
+        }
+        // Error
+     /*   if (p->left->val==q->val || p->right->val==q->val)
+        {
+             return p;
+        }
+        if (q->left->val==p->val || q->right->val==p->val)
+        {
+             return q;
+        }*/
+        if (q->val<root->val && p->val>root->val)
+        {
+             return root;
+        }
+        if (q->val<root->val && p->val>root->val)
+        {
+             return root;
+        }
+        if (q->val<root->val && p->val<root->val)
+        {
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        if (q->val>root->val && p->val>root->val)
+        {
+            return lowestCommonAncestor(root->right,p,q);
+        }
+
+    }
+};
