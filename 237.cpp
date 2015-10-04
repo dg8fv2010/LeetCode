fc19bdf32438f68078869d9a7dd86620488c7d8a
dg8fv2010 <945920259@qq.com>
2015-10-04 19:56:47 +0800

    Code

---
+++ 237.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+ struct ListNode {
+ int val;
+ ListNode *next;
+ ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (node!=NULL)
+        {
+            node->val=node->next->val;
+            node->next=node->next->next;
+        }
+        
+    }
+};
---
+++ 283.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        int countZero=0;
+        while(i!=nums.size()-countZero)
+        {
+            if (nums[i]==0)
+            {
+                nums.erase(nums.begin()+i);
+                nums.push_back(0);
+                countZero++;
+            }
+            else
+            {
+             i++;
+            }
+        }
+    }
+};
+
+int main()
+{
+    return 0;
+}
