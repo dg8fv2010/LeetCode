2f37868509aa388e45c7a3cce85571c90c33d638
dg8fv2010 <945920259@qq.com>
2015-10-05 23:41:58 +0800

    Code

---
+++ 027.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int> ans(nums);
+        nums.clear();
+        int i=0,j=0;
+        while (i<ans.size())
+        {
+            if (ans[i]==val)
+            {
+                i++;
+            }
+            else
+            {
+                nums.push_back(ans[i]);
+                i++;
+            }
+        }
+        
+        return nums.size();
+    }
+};
---
+++ 101.cpp
@@ -0,0 +1,29 @@
+ struct TreeNode {
+ int val;
+ TreeNode *left;
+ TreeNode *right;
+ TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+       if (root==NULL)
+       {
+           return true;
+       }
+       return check(root->left,root->right);
+    }
+       bool check(TreeNode* l, TreeNode*  r)
+       {
+           if (l==NULL && r==NULL)
+           {
+               return true;
+           }
+           if (l==NULL || r==NULL)
+           {
+               return false;
+           }
+           return (l->val==r->val) && check(l->left,r->right) && check(l->right,r->left);
+       }
+};
