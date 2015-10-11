044b2e5f887be707a1ff9e94f0edc9704ed00e93
dg8fv2010 <945920259@qq.com>
2015-10-11 22:55:09 +0800

    Code

---
+++ 203.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+
+struct ListNode {
+ int val;
+ ListNode *next;
+ ListNode(int x) : val(x), next(NULL) {}
+ };
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if (head==NULL)
+        {
+            return NULL;
+        }
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* p=dummy;
+        ListNode* q=head;
+        while (q!=NULL)
+        {
+             if (q->val==val)
+             {
+                 p->next=q->next;
+             }
+             else
+             {
+                  p=p->next;
+             }
+             q=q->next;
+        }
+        return dummy->next;
+    }
+};
+
+int main()
+{
+    ListNode* p=new ListNode(1);
+    Solution solu;
+    solu.removeElements(p,1);
+     return 0;
+}
