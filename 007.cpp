1901ac01b5de144e3d020a2210f349106be89ac6
dg8fv2010 <945920259@qq.com>
2015-10-12 23:42:39 +0800

    Code

---
+++ 007.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long sum=0;
+        while (x)
+        {
+            int temp=x%10;
+            sum=sum*10+temp;
+            if (sum>INT_MAX || sum<INT_MIN)
+            {
+                return 0;
+            }
+            x/=10;
+        }
+        return sum;
+    }
+};
