db6905564c5715854dfa4a97e68b522ca6613129
dg8fv2010 <945920259@qq.com>
2015-11-30 22:50:40 +0800

    Code

---
+++ 031.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int size=nums.size()-1;
+        for (int i=size-1;i>=0;i--)
+        {
+            if (nums[i+1]>nums[i])
+            {
+                int j=0;
+                for (j=size;j>=i-1;j--)
+                {
+                    if (nums[j]>nums[i])
+                    {
+                        break;
+                    }
+                }
+                swap(nums[i], nums[j]);
+                reverse(nums.begin()+i+1, nums.end());
+                return ;
+            }
+        }
+
+        reverse(nums.begin(), nums.end());
+        return ;
+    }
+};
